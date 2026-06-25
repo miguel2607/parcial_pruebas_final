0c84119eeaf091ce6868a0a9454f19911544f6eb
Miguel <marodriguez_141@cue.edu.co>
2026-06-25 17:36:20 -0500

    Agregar aplicacion principal

---
+++ src/main/java/com/ticketfast/TicketFastApplication.java
@@ -0,0 +1,12 @@
+package com.ticketfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketFastApplication.class, args);
+    }
+}
